ef0326a152ca3f1a796d2d3ad2da79fea32c9caa
yrwang1 <yw3598@columbia.edu>
2021-10-16 12:33:53 -0400

    Create global.R

---
+++ app/global.R
@@ -0,0 +1,35 @@
+if(!require(devtools)) install.packages("devtools", repos = "http://cran.us.r-project.org")
+if(!require(RSocrata)) devtools::install_github("Chicago/RSocrata")
+if(!require(fontawesome)) devtools::install_github("rstudio/fontawesome")
+if(!require(forcats)) install.packages("forcats", repos = "http://cran.us.r-project.org")
+if(!require(stringr)) install.packages("stringr", repos = "http://cran.us.r-project.org")
+if(!require(dplyr)) install.packages("dplyr", repos = "http://cran.us.r-project.org")
+if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+if(!require(RColorBrewer)) install.packages("RColorBrewer", repos = "http://cran.us.r-project.org")
+if(!require(leaflet)) install.packages("leaflet", repos = "http://cran.us.r-project.org")
+if(!require(plotly)) install.packages("plotly", repos = "http://cran.us.r-project.org")
+if(!require(geojsonio)) install.packages("geojsonio", repos = "http://cran.us.r-project.org")
+if(!require(shiny)) install.packages("shiny", repos = "http://cran.us.r-project.org")
+if(!require(shinyWidgets)) install.packages("shinyWidgets", repos = "http://cran.us.r-project.org")
+if(!require(shinydashboard)) install.packages("shinydashboard", repos = "http://cran.us.r-project.org")
+if(!require(shinythemes)) install.packages("shinythemes", repos = "http://cran.us.r-project.org")
+if(!require(here)) install.packages("here", repos = "http://cran.us.r-project.org")
+if(!require(readr)) install.packages("readr", repos = "http://cran.us.r-project.org")
+if(!require(tidyr)) install.packages("tidyr", repos = "http://cran.us.r-project.org")
+if(!require(ggmap)) install.packages("ggmap", repos = "http://cran.us.r-project.org")
+if(!require(googleway)) install.packages("googleway", repos = "http://cran.us.r-project.org")
+if(!require(viridis)) install.packages("viridis", repos = "http://cran.us.r-project.org")
+if(!require(htmltools)) install.packages("htmltools", repos = "http://cran.us.r-project.org")
+
+setwd(".")
+# ==================================================== Load data ==========================================================
+#nyc covid data
+
+covid <- read.csv("https://data.cityofnewyork.us/resource/rc75-m7u3.csv", stringsAsFactors = FALSE)
+covid$date_of_interest<-substr(covid$date_of_interest,1,10)
+covid<-covid[,!grepl("all_",colnames(covid))]
+covid<-covid[,!grepl("probable_death_count",colnames(covid))]
+covid <- covid %>%rename_all(funs(str_replace_all(., "_7day_avg", " (7day_avg)")))%>%
+  dplyr::select(-death_count_probable,-incomplete)
+nyc_latest <- covid %>% tail(1)
+nyc_yesterday<- covid[nrow(covid)-1,]
